4a0e7d9132c572c9fc5cb1ab627181a8e9e6549a
Isank <isankagarwal@gmail.com>
2019-12-28 16:53:46 +0530

    add quotes service

---
+++ quotes/src/test/java/com/service/quotes/QuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
