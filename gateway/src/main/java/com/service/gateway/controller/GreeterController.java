dccf7a63c2ea47bfe171a8732fd02a622080576c
Isank <isankagarwal@gmail.com>
2019-12-29 19:50:20 +0530

    add greet endpoint

--- gateway/src/main/java/com/service/gateway/controller/GreeterController.java
+++ gateway/src/main/java/com/service/gateway/controller/GreeterController.java
@@ -4,10 +4,12 @@ import com.service.gateway.service.GreeterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("greet")
+@RestController
+@RequestMapping("greet")
 @RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class GreeterController {
 
